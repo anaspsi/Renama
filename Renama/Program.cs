5be92d505eecdfb9ad3fa240dfa7b39586baf288
Ana Suryana di PSI <ana.suryana@smt.co.id>
2024-01-05 10:52:22 +0700

    Fix : error parse string

--- Renama/Program.cs
+++ Renama/Program.cs
@@ -1,58 +1,80 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
-
-Console.WriteLine("Please backup your data before proceed !");
 DirectoryInfo di = new DirectoryInfo(@".\");
 FileSystemInfo[] fsi = di.GetFileSystemInfos();
 
-IEnumerable<FileSystemInfo> sortAscendingQuery = from ffile in fsi
-                                                 where ffile.Name.ToUpper().Contains("PDF")
-                                                 orderby int.Parse(Path.GetFileNameWithoutExtension(ffile.Name))
-                                                 select ffile;
 int nomor = 1;
-bool isTobeRenamedExist = false; 
-foreach (var file in sortAscendingQuery)
+int invalidNomor = 0;
+foreach (var i in fsi)
 {
-    string fileName = Path.GetFileNameWithoutExtension(file.Name);
-    if (fileName != nomor.ToString())
+    if (i.Name.ToUpper().Contains("PDF"))
     {
-        Console.WriteLine(file.Name + " will be rename to " + nomor.ToString() + ".PDF");
-        isTobeRenamedExist = true;
-    }
-    else
-    {
-        Console.WriteLine(file.Name + " no need to be renamed");
+        string nameWithoutExtension = Path.GetFileNameWithoutExtension(i.Name);
+        if (int.TryParse(nameWithoutExtension, out _))
+        {
+
+        }
+        else
+        {
+            Console.WriteLine("the following file name is invalid," + nameWithoutExtension);
+            invalidNomor = nomor;
+        }
+
     }
     nomor++;
 }
-
-nomor = 1;
-
-if(isTobeRenamedExist)
+if (invalidNomor == 0)
 {
-    Console.Write("Do you agree with condition above ? [Y/N] :");
-    string key = Console.ReadLine();
-    if (key.ToUpper().Equals("Y"))
+    Console.WriteLine("Please backup your data before proceed !");
+    IEnumerable<FileSystemInfo> sortAscendingQuery = from ffile in fsi
+                                                     where ffile.Name.ToUpper().Contains("PDF")
+                                                     orderby int.Parse(Path.GetFileNameWithoutExtension(ffile.Name))
+                                                     select ffile;
+    nomor = 1;
+    bool isTobeRenamedExist = false;
+    foreach (var file in sortAscendingQuery)
     {
-        foreach (var file in sortAscendingQuery)
+        string fileName = Path.GetFileNameWithoutExtension(file.Name);
+        if (fileName != nomor.ToString())
         {
-            string fileName = Path.GetFileNameWithoutExtension(file.Name);
-            if (fileName != nomor.ToString())
-            {
-                File.Move(file.FullName, @".\" + nomor.ToString() + ".PDF");
-            }
-            nomor++;
+            Console.WriteLine(file.Name + " will be rename to " + nomor.ToString() + ".PDF");
+            isTobeRenamedExist = true;
         }
-        Console.Write("Done");
+        else
+        {
+            Console.WriteLine(file.Name + " no need to be renamed");
+        }
+        nomor++;
+    }
+
+    nomor = 1;
+
+    if (isTobeRenamedExist)
+    {
+        Console.Write("Do you agree with condition above ? [Y/N] :");
+        string key = Console.ReadLine();
+        if (key.ToUpper().Equals("Y"))
+        {
+            foreach (var file in sortAscendingQuery)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(file.Name);
+                if (fileName != nomor.ToString())
+                {
+                    File.Move(file.FullName, @".\" + nomor.ToString() + ".PDF");
+                }
+                nomor++;
+            }
+            Console.Write("Done");
+        }
+        else
+        {
+            Console.Write("Thanks");
+        }
     }
     else
     {
-        Console.Write("Thanks");
+        Console.Write("Nothing");
     }
-} else
-{
-    Console.Write("Nothing");
 }
 
 Console.ReadKey();
